12a85483ddb6c83a3ec0a5ab1452d3d3f4a2e0d0
Tamas Nepusz <ntamas@gmail.com>
2020-06-04 18:02:20 +0200

    bumped version to 1.7.2

--- src/flockwave/connections/version.py
+++ src/flockwave/connections/version.py
@@ -1,4 +1,4 @@
 """Version information for the Flockwave connections package."""
 
-__version_info__ = (1, 7, 1)
+__version_info__ = (1, 7, 2)
 __version__ = ".".join("{0}".format(i) for i in __version_info__)
