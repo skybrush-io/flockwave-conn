660553f48fc0d2b98bfdc12327a2e0eb1058d876
Tamas Nepusz <ntamas@gmail.com>
2020-06-22 19:32:23 +0200

    typo fix

--- src/flockwave/connections/servers.py
+++ src/flockwave/connections/servers.py
@@ -46,7 +46,7 @@ class UnixSocketListener(SocketListener):
         try:
             from socket import AF_UNIX
         except ImportError:
-            raise RuntimError("UNIX domain sockets are not supported on this platform")
+            raise RuntimeError("UNIX domain sockets are not supported on this platform")
 
         if os.path.exists(path) and not stat.S_ISSOCK(os.stat(path).st_mode):
             raise FileExistsError(f"Existing file is not a socket: {path}")
