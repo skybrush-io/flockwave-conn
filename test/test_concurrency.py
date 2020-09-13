1c64e35b5f232f0c555fdae55607c56067475e88
Tamas Nepusz <ntamas@gmail.com>
2020-09-13 12:49:26 +0200

    fixed AsyncBundler test with new pytest-trio

--- test/test_concurrency.py
+++ test/test_concurrency.py
@@ -5,64 +5,71 @@ from trio import WouldBlock, move_on_after, sleep
 from flockwave.concurrency import AsyncBundler, Future, FutureCancelled
 
 
-@trio_fixture
-def bundler():
-    bundler = AsyncBundler()
-    with move_on_after(10):
-        yield bundler
-
-
 class TestAsyncBundler:
-    async def test_yields_nothing_when_empty(self, bundler, autojump_clock):
-        async for bundle in bundler:
-            assert False, "bundler should not yield any bundles"
+    async def test_yields_nothing_when_empty(self, autojump_clock):
+        with move_on_after(10):
+            async for bundle in AsyncBundler():
+                assert False, "bundler should not yield any bundles"
 
-    async def test_yields_all_items_after_add(self, bundler, autojump_clock):
-        bundler.add(2)
-        bundler.add(3)
-        bundler.add(5)
-        async for bundle in bundler:
-            assert bundle == set([2, 3, 5])
+    async def test_yields_all_items_after_add(self, autojump_clock):
+        bundler = AsyncBundler()
+        with move_on_after(10):
+            bundler.add(2)
+            bundler.add(3)
+            bundler.add(5)
+            async for bundle in bundler:
+                assert bundle == set([2, 3, 5])
 
-    async def test_yields_all_items_after_add_many(self, bundler, autojump_clock):
-        bundler.add_many([2, 3, 5, 7])
-        bundler.add_many((11, 13))
-        async for bundle in bundler:
-            assert bundle == set([2, 3, 5, 7, 11, 13])
+    async def test_yields_all_items_after_add_many(self, autojump_clock):
+        bundler = AsyncBundler()
+        with move_on_after(10):
+            bundler.add_many([2, 3, 5, 7])
+            bundler.add_many((11, 13))
+            async for bundle in bundler:
+                assert bundle == set([2, 3, 5, 7, 11, 13])
 
-    async def test_clears_items_after_yielding(self, bundler, autojump_clock):
-        bundler.add_many([2, 3, 5, 7])
-        async for bundle in bundler:
-            assert bundle == set([2, 3, 5, 7])
-            break
-        bundler.add_many((11, 13))
-        async for bundle in bundler:
-            assert bundle == set([11, 13])
+    async def test_clears_items_after_yielding(self, autojump_clock):
+        bundler = AsyncBundler()
+        with move_on_after(10):
+            bundler.add_many([2, 3, 5, 7])
+            async for bundle in bundler:
+                assert bundle == set([2, 3, 5, 7])
+                break
+            bundler.add_many((11, 13))
+            async for bundle in bundler:
+                assert bundle == set([11, 13])
 
-    async def test_clears_items_before_yielding(self, bundler, autojump_clock):
-        bundler.add_many([2, 3, 5, 7])
-        bundler.clear()
-        async for bundle in bundler:
-            assert bundle == set()
-            break
+    async def test_clears_items_before_yielding(self, autojump_clock):
+        bundler = AsyncBundler()
+        with move_on_after(10):
+            bundler.add_many([2, 3, 5, 7])
+            bundler.clear()
+            async for bundle in bundler:
+                assert bundle == set()
+                break
 
-        bundler.add_many([2, 3, 5, 7])
-        bundler.clear()
-        bundler.add_many([11, 13])
-        async for bundle in bundler:
-            assert bundle == set([11, 13])
+            bundler.add_many([2, 3, 5, 7])
+            bundler.clear()
+            bundler.add_many([11, 13])
+            async for bundle in bundler:
+                assert bundle == set([11, 13])
 
-    async def test_filters_duplicates(self, bundler, autojump_clock):
-        bundler.add_many([2, 3, 3, 5, 5, 5, 7])
-        async for bundle in bundler:
-            assert bundle == set([2, 3, 5, 7])
-            break
-        bundler.add_many((2, 2, 3, 11))
-        async for bundle in bundler:
-            assert bundle == set([2, 3, 11])
+    async def test_filters_duplicates(self, autojump_clock):
+        bundler = AsyncBundler()
+        with move_on_after(10):
+            bundler.add_many([2, 3, 3, 5, 5, 5, 7])
+            async for bundle in bundler:
+                assert bundle == set([2, 3, 5, 7])
+                break
+            bundler.add_many((2, 2, 3, 11))
+            async for bundle in bundler:
+                assert bundle == set([2, 3, 11])
 
-    async def test_separated_producer_consumer(self, bundler, autojump_clock, nursery):
-        async def producer():
+    async def test_separated_producer_consumer(self, autojump_clock, nursery):
+        bundler = AsyncBundler()
+
+        async def producer(task_status):
+            task_status.started()
             items = list(range(10))
             for item in items:
                 bundler.add(item)
@@ -70,21 +77,25 @@ class TestAsyncBundler:
 
         async def consumer():
             bundles = []
-            async for bundle in bundler:
-                bundles.append(bundle)
-                await sleep(0.5)
+            with move_on_after(10):
+                async for bundle in bundler:
+                    bundles.append(bundle)
+                    await sleep(0.5)
             return bundles
 
-        nursery.start_soon(producer)
+        await nursery.start(producer)
         bundles = await consumer()
 
-        assert len(bundles) == 4
-        assert bundles[0] == (0, 1, 2)
-        assert bundles[1] == (3, 4)
-        assert bundles[2] == (5, 6, 7)
-        assert bundles[3] == (8, 9)
+        assert len(bundles) == 5
+        assert bundles[0] == {0}
+        assert bundles[1] == {1, 2}
+        assert bundles[2] == {3, 4}
+        assert bundles[3] == {5, 6, 7}
+        assert bundles[4] == {8, 9}
 
-    async def test_multiple_consumers(self, bundler, autojump_clock, nursery):
+    async def test_multiple_consumers(self, autojump_clock, nursery):
+        bundler = AsyncBundler()
+
         async def consumer():
             return [bundle async for bundle in bundler]
 
