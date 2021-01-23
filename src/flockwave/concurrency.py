5cf3cd2eb06a9f524b18ab283e764fcd361bd707
Tamas Nepusz <ntamas@gmail.com>
2021-01-23 17:00:32 +0100

    feat: moved race(), delayed() and aclosing() here, added CancellableTaskGroup

--- src/flockwave/concurrency.py
+++ src/flockwave/concurrency.py
@@ -3,14 +3,30 @@
 from collections.abc import Mapping
 from contextlib import asynccontextmanager
 from functools import partial, wraps
-from trio import Cancelled, Event, WouldBlock
+from inspect import iscoroutine, iscoroutinefunction
+from trio import Cancelled, CancelScope, Event, open_nursery, Nursery, WouldBlock, sleep
 from trio_util import MailboxRepeatedEvent
-from typing import Any, Callable, Generic, Iterable, Iterator, TypeVar
+from typing import Any, Callable, Dict, Generic, Iterable, Iterator, TypeVar
 
-__all__ = ("AsyncBundler", "cancellable", "Future", "FutureCancelled")
+__all__ = ("AsyncBundler", "aclosing", "cancellable", "Future", "FutureCancelled")
 
 
 T = TypeVar("T")
+
+
+class aclosing:
+    """Context manager that closes an async generator when the context is
+    exited. Similar to `closing()` in `contextlib`.
+    """
+
+    def __init__(self, aiter):
+        self._aiter = aiter
+
+    async def __aenter__(self):
+        return self._aiter
+
+    async def __aexit__(self, *args):
+        await self._aiter.aclose()
 
 
 def cancellable(func):
@@ -24,6 +40,68 @@ def cancellable(func):
             return await func(*args, **kwds)
 
     decorated._cancellable = True
+
+    return decorated
+
+
+def _identity(obj: Any) -> Any:
+    """Identity function that returns its input argument."""
+    return obj
+
+
+def delayed(seconds: float, fn=None, *, ensure_async: bool = False):
+    """Decorator or decorator factory that delays the execution of a
+    synchronous function, coroutine or coroutine-returning function with a
+    given number of seconds.
+
+    Parameters:
+        seconds: the number of seconds to delay with. Negative numbers will
+            throw an exception. Zero will return the identity function.
+        fn: the function, coroutine or coroutine-returning function to delay
+        ensure_async: when set to `True`, synchronous functions will automatically
+            be converted to asynchronous before delaying them. This is needed
+            if the delayed function is going to be executed in the async
+            event loop because a synchronous function that sleeps will block
+            the entire event loop.
+
+    Returns:
+        the delayed function, coroutine or coroutine-returning function
+    """
+    if seconds < 0:
+        raise ValueError("delay must not be negative")
+
+    if seconds == 0 and not ensure_async:
+        return _identity if fn is None else fn
+
+    if fn is None:
+        return partial(delayed, seconds, ensure_async=ensure_async)
+
+    if iscoroutinefunction(fn):
+
+        @wraps(fn)
+        async def decorated(*args, **kwds):
+            await sleep(seconds)
+            return await fn(*args, **kwds)
+
+    elif iscoroutine(fn):
+
+        async def decorated():
+            await sleep(seconds)
+            return fn
+
+        decorated = decorated()
+
+    elif ensure_async:
+
+        async def decorated(*args, **kwds):
+            await sleep(seconds)
+            return fn(*args, **kwds)
+
+    else:
+
+        def decorated(*args, **kwds):
+            sleep(seconds)
+            return fn(*args, **kwds)
 
     return decorated
 
@@ -93,6 +171,34 @@ class AsyncBundler:
         finally:
             if it:
                 await it.aclose()
+
+
+class CancellableTaskGroup:
+    """Object representing a group of tasks running in an associated nursery
+    that can be cancelled with a single method call.
+    """
+
+    def __init__(self, nursery: Nursery):
+        """Constructor.
+
+        Parameters:
+            nursery: the nursery that the tasks of the task group will be
+                executed in.
+        """
+        self._nursery = nursery
+        self._cancel_scopes = []
+
+    def cancel_all(self) -> None:
+        """Cancels all tasks running in the task group."""
+        for cancel_scope in self._cancel_scopes:
+            cancel_scope.cancel()
+        del self._cancel_scopes[:]
+
+    def start_soon(self, func, *args):
+        """Starts a new task in the task group."""
+        cancel_scope = CancelScope()
+        self._cancel_scopes.append(cancel_scope)
+        self._nursery.start_soon(partial(cancellable(func), cancel_scope=cancel_scope))
 
 
 class FutureCancelled(RuntimeError):
@@ -322,3 +428,32 @@ class FutureMap(Mapping, Generic[T]):
 
         self._futures[id] = future = self._factory()
         return self.Context(future, partial(self._dispose_future, id, future))
+
+
+async def race(funcs: Dict[str, Callable[[], Any]]):
+    """Run multiple async functions concurrently and wait for at least one of
+    them to complete. Return the key corresponding to the function and the
+    result of the function as well.
+    """
+    holder = []
+
+    async with open_nursery() as nursery:
+        cancel = nursery.cancel_scope.cancel
+        for key, func in funcs.items():
+            set_result = partial(_cancel_and_set_result, cancel, holder, key)
+            nursery.start_soon(_wait_and_call, func, set_result)
+
+    return holder[0]
+
+
+def _cancel_and_set_result(cancel, holder, key, value):
+    holder.append((key, value))
+    cancel()
+
+
+async def _wait_and_call(f1, f2) -> None:
+    """Call an async function f1() and wait for its result. Call synchronous
+    function `f2()` with the result of `f1()` when `f1()` returns.
+    """
+    result = await f1()
+    f2(result)
--- test/test_concurrency.py
+++ test/test_concurrency.py
@@ -1,7 +1,82 @@
+from inspect import signature
 from pytest import raises
 from trio import WouldBlock, move_on_after, sleep
 
-from flockwave.concurrency import AsyncBundler, Future, FutureCancelled
+from flockwave.concurrency import (
+    aclosing,
+    cancellable,
+    delayed,
+    race,
+    AsyncBundler,
+    CancellableTaskGroup,
+    Future,
+    FutureCancelled,
+)
+
+
+async def test_aclosing(autojump_clock):
+    async def wait():
+        for i in range(100):
+            await sleep(1)
+            yield i
+
+    items = []
+    gen = wait()
+    with move_on_after(4.5):
+        async with aclosing(gen):
+            async for item in gen:
+                items.append(item)
+
+    assert items == [0, 1, 2, 3]
+
+
+async def test_cancellable(autojump_clock):
+    test_var = 42
+
+    async def func(foo, bar: int = 42, *, baz):
+        nonlocal test_var
+        await sleep(5)
+        test_var = 84
+
+    cancellable_func = cancellable(func)
+
+    sig = signature(cancellable_func, follow_wrapped=False)
+    assert str(sig) == "(*args, cancel_scope, **kwds)"
+
+    sig = signature(func, follow_wrapped=False)
+    assert signature(cancellable_func, follow_wrapped=True) == signature(func)
+
+
+def test_delayed():
+    def sync_func(a):
+        return a + 5
+
+    with raises(ValueError):
+        delayed(-3)
+
+    assert delayed(0)(sync_func) == sync_func
+    assert delayed(0, sync_func) == sync_func
+
+
+async def test_race(autojump_clock):
+    terminated = []
+
+    def make_func(x):
+        nonlocal terminated
+
+        def func():
+            terminated.append(x)
+            return x
+
+        return func
+
+    func1 = delayed(2, make_func(3), ensure_async=True)
+    func2 = delayed(3, make_func(7), ensure_async=True)
+    func3 = delayed(5, make_func(2), ensure_async=True)
+
+    result = await race({"foo": func1, "bar": func2, "baz": func3})
+    assert result == ("foo", 3)
+    assert terminated == [3]
 
 
 class TestAsyncBundler:
@@ -121,6 +196,60 @@ class TestAsyncBundler:
             await consumer()
 
         assert "can only have one listener" in str(ex.value)
+
+
+class TestCancellableTaskGroup:
+    async def test_cancel_empty(self, nursery):
+        tg = CancellableTaskGroup(nursery)
+        tg.cancel_all()
+        assert True
+
+    async def test_cancel_multiple(self, nursery):
+        tg = CancellableTaskGroup(nursery)
+        tg.cancel_all()
+        tg.cancel_all()
+        tg.cancel_all()
+        assert True
+
+    async def test_start_and_cancel(self, autojump_clock, nursery):
+        ended1, ended2 = False, False
+
+        async def task1():
+            nonlocal ended1
+            await sleep(5)
+            ended1 = True
+
+        async def task2():
+            nonlocal ended2
+            await sleep(3)
+            ended2 = True
+
+        await task1()
+        assert ended1
+
+        await task2()
+        assert ended2
+
+        ended1, ended2 = False, False
+
+        tg = CancellableTaskGroup(nursery)
+        tg.start_soon(task1)
+        tg.start_soon(task2)
+        await sleep(2)
+        tg.cancel_all()
+        assert not ended1 and not ended2
+
+        ended1, ended2 = False, False
+
+        tg = CancellableTaskGroup(nursery)
+        tg.start_soon(task1)
+        tg.start_soon(task2)
+        await sleep(4)
+        assert ended2 and not ended1
+        tg.cancel_all()
+
+        await sleep(5)
+        assert ended2 and not ended1
 
 
 class TestFuture:
