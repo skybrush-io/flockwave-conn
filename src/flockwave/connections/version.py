2a78aa332f039ed7774fd65e1c82f8bee63b5f4f
Tamas Nepusz <ntamas@gmail.com>
2021-10-01 00:51:06 +0200

    chore: bumped version to 3.0.2

--- src/flockwave/connections/version.py
+++ src/flockwave/connections/version.py
@@ -1,4 +1,4 @@
 """Version information for the Flockwave connections package."""
 
-__version_info__ = (3, 0, 1)
+__version_info__ = (3, 0, 2)
 __version__ = ".".join("{0}".format(i) for i in __version_info__)
