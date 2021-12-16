00f290bf67d54c4393a65013ad53a4a43956bd5b
Tamas Nepusz <ntamas@gmail.com>
2021-12-16 09:40:33 +0100

    chore: type improvement

--- src/flockwave/channels/encoder.py
+++ src/flockwave/channels/encoder.py
@@ -3,6 +3,8 @@ and writes them to a WritableConnection_.
 """
 
 from inspect import iscoroutinefunction
+from typing import Awaitable, Callable
+
 from trio.abc import SendChannel
 
 from .types import Encoder, MessageType, RawType, Writer
@@ -13,13 +15,15 @@ class EncoderChannel(SendChannel[MessageType]):
     WritableConnection_.
     """
 
+    _writer: Callable[..., Awaitable[None]]
+
     def __init__(self, writer: Writer[RawType], encoder: Encoder[MessageType, RawType]):
         if iscoroutinefunction(getattr(writer, "write", None)):
             # Writer is a Connection
-            self._writer = writer.writer
+            self._writer = writer.writer  # type: ignore
             self._closer = getattr(writer, "close", None)
         elif iscoroutinefunction(writer):
-            self._writer = writer
+            self._writer = writer  # type: ignore
             self._closer = None
         else:
             raise TypeError(
